f322722e40c6f6d65984ec2110b5fba6e7417d4b
DuvanMVP <158522532+DuvanMVP@users.noreply.github.com>
2024-03-14 17:56:00 -0500

    Add files via upload

---
+++ app_funciones.py
@@ -0,0 +1,10 @@
+import os
+def fnt_limpiar():
+    os.system('cls')
+def fnt_mensaje(msn):
+    fnt_limpiar()
+    print('El mensaje es: ', msn)
+    
+    
+    
+fnt_mensaje('El burrito sabanero')
